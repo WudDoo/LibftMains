675235efef8bb5118cd8695d10b1b8eaa9417b66
mortins- <mortins-@c1r18s14.42lisboa.com>
2022-10-28 16:29:48 +0100

    strlcat unfin

--- mains/ft_strlcatmain.c
+++ mains/ft_strlcatmain.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpymain.c                                   :+:      :+:    :+:   */
+/*   ft_strlcatmain.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/28 15:02:33 by mortins-          #+#    #+#             */
-/*   Updated: 2022/10/28 16:10:51 by mortins-         ###   ########.fr       */
+/*   Created: 2022/10/28 15:07:51 by mortins-          #+#    #+#             */
+/*   Updated: 2022/10/28 16:24:41 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,18 @@
 int	main(int argc, char **argv)
 {
 	long long unsigned int	a;
-	int						b;
 
 	if (argc != 4)
 		return (0);
 	a = atoi(argv[3]);
-	b = ft_strlcpy(argv[1], argv[2], a);
-	printf("src: %s\ndest: %s\nsrc_size: %d\n", argv[2], argv[1], b);
+	printf("Before using the function\n");
+	printf("src: %s\n", argv[2]);
+	printf("dst: %s\n", argv[1]);
+	printf("dstsize: %s\n", argv[3]);
+	ft_strlcat(argv[1], (const char *)argv[2], a);
+	printf("After using the function\n");
+	printf("src: %s\n", argv[2]);
+	printf("dst: %s\n", argv[1]);
+	printf("dstsize: %d\n", ft_strlcat(argv[1], (const char *)argv[2], a));
 	return (0);
 }
